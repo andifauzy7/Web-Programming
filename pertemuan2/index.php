49754e0ee8a4791b95e72348c8fbb5e507b91640
andifauzy7 <andifauzy00@gmail.com>
2019-06-01 18:39:52 +0700

    Catatan mengenai Syntax PHP. (Note about Syntax PHP)

---
+++ pertemuan2/index.php
@@ -0,0 +1,28 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Ouput
+// Gunakan echo, print, print_r (mencetak isi array), var_dump (Melihat isi variabel)
+
+// Penulisan sintaks PHP
+// 1. PHP didalam HTML
+// 2. HTML didalam PHP
+
+// Variabel dan Tipe Data
+// Tidak boleh diawali angka, boleh mengandung angka
+// $(namavar)
+// Perbedaan " dan ' .. " Menampilkan nilai variabel.
+
+// Operator
+// Aritmatika + - * / %
+// Penggabung String (.) 
+	/*$namadepan = "Andi";
+	$namabelakang = "Dewantara";
+	echo $namadepan . " " .$namabelakang; */
+// Assignment = += -= /= %= *= .= !=
+// Perbandingan < > <= >= == (HANYA NILAINYA SAJA)
+// Jika dengan tipe data ===, !==
+// &&, ||, !
+
+?>
